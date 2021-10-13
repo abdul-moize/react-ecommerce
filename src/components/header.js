079d77472a7abd359deda0a370a1d54bb17a3607
abdul-moize <abdul.moize@arbisoft.com>
2021-10-13 16:26:49 +0500

    remove bubble code

--- src/components/header.js
+++ src/components/header.js
@@ -4,25 +4,19 @@ import { auth, homepage } from "./constants";
 import { isContentManager } from "./utils";
 import "./header.css"
 function Header(props) {
-  const bubble = useRef()
   let content = (<Link to={auth}>Log In</Link>)
   const isLoggedIn = props.isLoggedIn
-  const added = localStorage.getItem("added")
   if (isLoggedIn) {
     content = (
       <div>
         {isContentManager() ? <Link to="/add" className="active" href="">Add Product</Link> : null}
         <Link to="/orders"> Orders</Link>
         <Link style={{padding: '0px'}} to="/cart"> <img className="cart-image" src="/cart_image.png" alt="Cart" /></Link>
-        {added ? <div id="message" ref={bubble} className="bubble bubble-top-right">Product added to cart</div> : null}
         <Link to="/profile"> Profile</Link>
         <Link to="/logout"> Log Out</Link >
       </div>
     )
-    setTimeout(() => {
-      if (bubble.current)
-        bubble.current.hidden = true
-    }, 2000)
+    
   }
 
   return (
