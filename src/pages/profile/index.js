3d337084935572376939a4aba26bfb04e3af027c
abdul-moize <abdul.moize@arbisoft.com>
2021-10-22 19:35:16 +0500

    refactor: convert class component to functional component

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,139 +1,133 @@
-import React from "react";
-import { withRouter } from "react-router";
-import { SubmitButton } from "../../components/elements";
+import React, { useEffect } from "react";
+import { useHistory } from "react-router";
+import { useState } from "react/cjs/react.development";
+import { ErrorField, SubmitButton } from "../../components/elements";
 import { AUTH } from "../../constants";
 import { getUserData, updateUserData } from "../../services/userService";
 import "./profile.css";
 
-class Profile extends React.Component {
-  constructor(props) {
-    super(props);
+function Profile() {
+  const history = useHistory();
 
-    this.state = {
-      name: "",
-      email: "",
-      password: "",
-      nameValid: true,
-      emailValid: true,
-      passwordValid: true,
-    };
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const [errors, setErrors] = useState({});
 
-    this.formRef = React.createRef();
-
-    this.handleNameChange = this.handleNameChange.bind(this);
-    this.handleEmailChange = this.handleEmailChange.bind(this);
-    this.handlePasswordChange = this.handlePasswordChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleNameChange(event) {
+  const setValuesAndErrors = (newValues, newErrors) => {
+    setValues(() => newValues);
+    setErrors(() => newErrors);
+  };
+  const onNameChange = (event) => {
     const newName = event.target.value;
+    const nameError = newName.trim() === "" && "Name can not be blank";
 
-    this.setState({
-      name: newName,
-      nameValid: this.state.valid && newName.trim() !== "",
-    });
-  }
+    setValuesAndErrors(
+      { ...values, name: newName },
+      { ...errors, name: nameError }
+    );
+  };
 
-  handleEmailChange(event) {
-    const newEmail = event.target.value;
+  const onEmailChange = (event) => {
     const emailRegex = /\w*@\w*.com\b/;
 
-    this.setState({
-      email: newEmail,
-      emailValid: emailRegex.test(newEmail),
-    });
-  }
+    const newEmail = event.target.value;
+    const emailError = !emailRegex.test(newEmail) && "Enter a valid email";
 
-  handlePasswordChange(event) {
-    const newPassword = event.target.value;
+    setValuesAndErrors(
+      { ...values, email: newEmail },
+      { ...errors, email: emailError }
+    );
+  };
+
+  const onPasswordChange = (event) => {
     const passwordRegex = /[0-9]+[a-zA-z]+\w{6}|[a-zA-Z]+[0-9]+\w{6}/;
 
-    this.setState({
-      password: newPassword,
-      passwordValid: passwordRegex.test(newPassword),
-    });
-  }
+    const newPassword = event.target.value;
+    const passwordError =
+      !passwordRegex.test(newPassword) && "Enter a valid password";
 
-  componentDidMount() {
+    setValuesAndErrors(
+      { ...values, password: newPassword },
+      { ...errors, password: passwordError }
+    );
+  };
+  useEffect(() => {
     getUserData()
       .then((userData) => {
-        this.setState({
+        setValues(() => ({
           name: userData.name,
           email: userData.email,
-        });
+        }));
       })
       .catch((errorMessage) => {
-        if (errorMessage === "You are not logged in")
-          this.props.history.replace(AUTH);
+        if (errorMessage === "You are not logged in") history.replace(AUTH);
       });
-  }
+  }, [history]);
 
-  handleSubmit(event) {
+  const onSubmit = (event) => {
     event.preventDefault();
-
-    if (!this.state.nameValid) alert("Name can not be blank");
-    if (!this.state.emailValid) alert("Please enter a valid email");
-    if (!this.state.passwordValid) alert("Please enter a valid password");
-
-    if (
-      this.state.nameValid &&
-      this.state.emailValid &&
-      this.state.passwordValid
-    ) {
-      const userData = new FormData(this.formRef.current);
-      if (userData.get("password") === "") userData.delete("password");
+    if (!errors.name && !errors.email && !errors.password) {
+      const userData = new FormData();
+      for (let key in values) {
+        if (values[key] !== "") userData.append(key, values[key]);
+      }
       updateUserData(userData)
         .then((res) => alert(res.message))
         .catch((exception) => alert(exception));
     }
-  }
+  };
 
-  render() {
-    return (
-      <div className="profile-box">
-        <h1 className="profile-title">Profile</h1>
-        <form
-          className="profile-form"
-          ref={this.formRef}
-          onSubmit={this.handleSubmit}
-        >
-          <label className="profile-form-row" htmlFor="name">
-            <h3 className="profile-form-label">Name:</h3>
-            <input
-              className="profile-form-field"
-              name="name"
-              onChange={this.handleNameChange}
-              placeholder="Enter Name"
-              defaultValue={this.state.name}
-            />
-          </label>
-          <label className="profile-form-row" htmlFor="email">
-            <h3 className="profile-form-label">Email:</h3>
-            <input
-              className="profile-form-field"
-              name="email"
-              onChange={this.handleEmailChange}
-              placeholder="Enter Email"
-              defaultValue={this.state.email}
-            />
-          </label>
-          <label className="profile-form-row" htmlFor="password">
-            <h3 className="profile-form-label">Password:</h3>
-            <input
-              className="profile-form-field"
-              type="password"
-              name="password"
-              onChange={this.handlePasswordChange}
-              placeholder="Enter New Password"
-              defaultValue={this.state.password}
-            />
-          </label>
-          <SubmitButton text="Save Changes" />
-        </form>
-      </div>
-    );
-  }
+  return (
+    <div className="profile-box">
+      <h1 className="profile-title">Profile</h1>
+      <form className="profile-form" onSubmit={onSubmit}>
+        <label className="profile-form-row" htmlFor="name">
+          <h3 className="profile-form-label">Name:</h3>
+          <input
+            className="profile-form-field"
+            name="name"
+            onChange={onNameChange}
+            placeholder="Enter Name"
+            defaultValue={values.name}
+          />
+        </label>
+        {errors.name && (
+          <ErrorField style={{ fontSize: "20px" }} text={errors.name} />
+        )}
+        <label className="profile-form-row" htmlFor="email">
+          <h3 className="profile-form-label">Email:</h3>
+          <input
+            className="profile-form-field"
+            name="email"
+            onChange={onEmailChange}
+            placeholder="Enter Email"
+            defaultValue={values.email}
+          />
+        </label>
+        {errors.email && (
+          <ErrorField style={{ fontSize: "20px" }} text={errors.email} />
+        )}
+        <label className="profile-form-row" htmlFor="password">
+          <h3 className="profile-form-label">Password:</h3>
+          <input
+            className="profile-form-field"
+            type="password"
+            name="password"
+            onChange={onPasswordChange}
+            placeholder="Enter New Password"
+            defaultValue={values.password}
+          />
+        </label>
+        {errors.password && (
+          <ErrorField style={{ fontSize: "20px" }} text={errors.password} />
+        )}
+        <SubmitButton text="Save Changes" />
+      </form>
+    </div>
+  );
 }
 
-export default withRouter(Profile);
+export default Profile;
